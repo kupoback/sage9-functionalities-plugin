7e9e31147c2f097526a9b5491e1901cb4f9c4a57
Nick Makris <>
2019-04-24 09:45:37 -0500

    Made some changes to the js file to fit with ESLint standards

--- admin/js/cf-admin.js
+++ admin/js/cf-admin.js
@@ -1,82 +1,77 @@
-(function ($) {
-	'use strict';
-	
-	let mediaUploader;
-	
-	$('[data-name="hidden-media"]').each(function () {
-		if (this.value) {
-			$(this).siblings('.no-value').addClass('hide');
-			$(this).siblings('.has-value').removeClass('hide');
-		}
-		else {
-			$(this).siblings('.no-value').removeClass('hide');
-			$(this).siblings('.has-value').addClass('hide');
-		}
-	});
-	
-	$('#cf_plugin_media-container [data-name="add"], #cf_plugin_media-container [data-name="edit"]').on('click', function (e) {
-		let $this = $(this);
-		
-		e.preventDefault();
-		
-		mediaUploader.open();
-		
-		// eslint-disable-next-line no-multi-assign
-		mediaUploader = wp.media.frames.file_frame = wp.media({
-			title:    'Choose Media',
-			button:   {
-				text: 'Choose Media'
-			},
-			multiple: false
-		});
-		
-		mediaUploader.on('select', function () {
-			let attachment = mediaUploader.state().get('selection').first().toJSON();
-			let thisContainer = $this.parents('#cf_plugin_media-container');
-			let dataMedia = $(thisContainer).find('[data-name="media"]');
-			let dataHidden = $(thisContainer).find('[data-name="hidden-media"]');
-			
-			dataHidden.attr('value', attachment.id);
-			
-			if (thisContainer.find('.image').length !== 0) {
-				dataMedia.attr('src', attachment.sizes.thumbnail.url);
-				dataMedia.attr('data-src', attachment.sizes.thumbnail.url);
-			}
-			
-			if (thisContainer.find('.file-name').length !== 0) {
-				dataMedia.find('span').text(attachment.title);
-			}
-			
-			// Toggle the divs
-			thisContainer.children('.no-value').addClass('hide');
-			thisContainer.children('.has-value').removeClass('hide');
-		});
-		mediaUploader.open();
-	});
-	
-	$('[data-name="remove"]').on('click', function (e) {
-		let $this = $(this);
-		let thisContainer = $this.parents('#cf_plugin_media-container');
-		let dataMedia = $(thisContainer).find('[data-name="media"]');
-		let dataHidden = $(thisContainer).find('[data-name="hidden-media"]');
-		
-		e.preventDefault();
-		
-		if (thisContainer.find('.image').length !== 0) {
-			dataMedia.attr('src', '');
-			dataMedia.attr('data-src', '');
-		}
-		
-		if (thisContainer.find('.file-name').length !== 0) {
-			dataMedia.find('span').text('');
-		}
-		
-		dataHidden.val('');
-		
-		// Toggle the divs
-		thisContainer.children('.no-value').removeClass('hide');
-		thisContainer.children('.has-value').addClass('hide');
-		
-	});
-	
-})(jQuery);
+( function( $ ) {
+  let mediaUploader;
+
+  $( "[data-name=\"hidden-media\"]" ).each( function toggleHide() {
+    if ( this.value ) {
+      $( this ).siblings( ".no-value" ).addClass( "hide" );
+      $( this ).siblings( ".has-value" ).removeClass( "hide" );
+    } else {
+      $( this ).siblings( ".no-value" ).removeClass( "hide" );
+      $( this ).siblings( ".has-value" ).addClass( "hide" );
+    }
+  } );
+
+  $( "#cf_plugin_media-container [data-name=\"add\"], #cf_plugin_media-container [data-name=\"edit\"]" ).on( "click", function clickAddEdit( e ) {
+    const $this = $( this );
+
+    e.preventDefault();
+
+    mediaUploader.open();
+
+    // eslint-disable-next-line no-multi-assign
+    mediaUploader = wp.media.frames.file_frame = wp.media( {
+      title: "Choose Media",
+      button: {
+        text: "Choose Media",
+      },
+      multiple: false,
+    } );
+
+    mediaUploader.on( "select", () => {
+      const attachment = mediaUploader.state().get( "selection" ).first().toJSON();
+      const thisContainer = $this.parents( "#cf_plugin_media-container" );
+      const dataMedia = $( thisContainer ).find( "[data-name=\"media\"]" );
+      const dataHidden = $( thisContainer ).find( "[data-name=\"hidden-media\"]" );
+
+      dataHidden.attr( "value", attachment.id );
+
+      if ( thisContainer.find( ".image" ).length !== 0 ) {
+        dataMedia.attr( "src", attachment.sizes.thumbnail.url );
+        dataMedia.attr( "data-src", attachment.sizes.thumbnail.url );
+      }
+
+      if ( thisContainer.find( ".file-name" ).length !== 0 ) {
+        dataMedia.find( "span" ).text( attachment.title );
+      }
+
+      // Toggle the divs
+      thisContainer.children( ".no-value" ).addClass( "hide" );
+      thisContainer.children( ".has-value" ).removeClass( "hide" );
+    } );
+    mediaUploader.open();
+  } );
+
+  $( "[data-name=\"remove\"]" ).on( "click", function clickRemove( e ) {
+    const $this = $( this );
+    const thisContainer = $this.parents( "#cf_plugin_media-container" );
+    const dataMedia = $( thisContainer ).find( "[data-name=\"media\"]" );
+    const dataHidden = $( thisContainer ).find( "[data-name=\"hidden-media\"]" );
+
+    e.preventDefault();
+
+    if ( thisContainer.find( ".image" ).length !== 0 ) {
+      dataMedia.attr( "src", "" );
+      dataMedia.attr( "data-src", "" );
+    }
+
+    if ( thisContainer.find( ".file-name" ).length !== 0 ) {
+      dataMedia.find( "span" ).text( "" );
+    }
+
+    dataHidden.val( "" );
+
+    // Toggle the divs
+    thisContainer.children( ".no-value" ).removeClass( "hide" );
+    thisContainer.children( ".has-value" ).addClass( "hide" );
+  } );
+}( jQuery ) );
--- gulpfile.js
+++ gulpfile.js
@@ -1,206 +1,191 @@
-/*======================================
-	Gulp Variables
-======================================*/
-const { src, dest, watch, series, parallel } = require( 'gulp' ),
-      del                                    = require( 'del' ),
-      pipeline                               = require( 'readable-stream' ).pipeline; // rm -rf
+// Gulp Variables
+const {
+  src,
+  dest,
+  watch,
+  series,
+  parallel,
+} = require( "gulp" );
 
-/*======================================
-	Gulp Plugins
-======================================*/
-const autoprefixer = require( 'autoprefixer' ),
-      babel        = require( 'gulp-babel' ),
-      concat       = require( 'gulp-concat' ),
-      cssnano      = require( 'cssnano' ),
-      eslint       = require( 'gulp-eslint' ),
-      globbing     = require( 'gulp-css-globbing' ),
-      gulpIf       = require( 'gulp-if' ),
-      flatten      = require( 'gulp-flatten' ),
-      plumber      = require( 'gulp-plumber' ),
-      postcss      = require( 'gulp-postcss' ),
-      rename       = require( 'gulp-rename' ),
-      sass         = require( 'gulp-sass' ),
-      sourcemaps   = require( 'gulp-sourcemaps' ),
-      terser       = require( 'gulp-terser' ),
-      uglify       = require( 'gulp-uglify' );
+// Gulp Plugins
+const autoprefixer = require( "autoprefixer" );
+const babel = require( "gulp-babel" );
+const concat = require( "gulp-concat" );
+const cssnano = require( "cssnano" );
+const eslint = require( "gulp-eslint" );
+const globbing = require( "gulp-css-globbing" );
+const gulpIf = require( "gulp-if" );
+const flatten = require( "gulp-flatten" );
+const plumber = require( "gulp-plumber" );
+const postcss = require( "gulp-postcss" );
+const rename = require( "gulp-rename" );
+const sass = require( "gulp-sass" );
+const sourcemaps = require( "gulp-sourcemaps" );
+const terser = require( "gulp-terser" );
+const uglify = require( "gulp-uglify" );
 
-/*======================================
-	Files and Functions
-======================================*/
-const _write = {
-          admin:  './admin/dist',
-          public: './public/dist',
-      },
-      _css   = {
-          admin:  {
-              fileName: 'cf-admin',
-              read:     './admin/css/cf-admin.scss',
-              watch:    './admin/css/**/*',
-          },
-          public: {
-              fileName: 'cf-public',
-              read:     './public/css/cf-public.scss',
-              watch:    './public/css/**/*',
-          },
-      },
-      _js    = {
-          admin:  {
-              fileName: 'cf-admin',
-              read:     './admin/js',
-              watch:    './admin/js/**/*',
-          },
-          public: {
-              fileName: 'cf-public',
-              read:     './public/js',
-              watch:    './public/js/**/*',
-              // read:     {
-              //     js: './public/js/**/*.js',
-              // },
-          },
-      };
+// Files and Functions
+const writeDec = {
+  admin: "./admin/dist",
+  public: "./public/dist",
+};
+const cssDec = {
+  admin: {
+    fileName: "cf-admin",
+    read: "./admin/css/cf-admin.scss",
+    watch: "./admin/css/**/*",
+  },
+  public: {
+    fileName: "cf-public",
+    read: "./public/css/cf-public.scss",
+    watch: "./public/css/**/*",
+  },
+};
+const jsDec = {
+  admin: {
+    fileName: "cf-admin",
+    read: "./admin/js",
+    watch: "./admin/js/**/*",
+  },
+  public: {
+    fileName: "cf-public",
+    read: "./public/js",
+    watch: "./public/js/**/*",
+  },
+};
 
-const adminCSS = sassCompile( _css.admin.read, './admin/dist' );
-const publicCSS = sassCompile( _css.public.read, _write.public );
-const adminJS = jsCompile( _js.admin.fileName, _js.admin.read, './admin/dist' );
-const publicJS = jsCompile( _js.public.fileName, _js.public.read, _write.public );
+//  Functions
 
-
-/*======================================
-	Functions
-======================================*/
-
-//<editor-fold desc="Functions">
-function clean ($path) {
-    return del( [ $path ] );
+//  <editor-fold desc="Functions">
+/**
+ * Our Error Handle
+ * @param {string} err Returns the error of the function
+ */
+function handleError( err ) {
+  console.log( err.toString() );
+  this.emit( "end" );
 }
 
 /**
- * Our Error Handle
- * @param err
+ * Description: Our checker to see if the file is fixed or not
+ * @param {string} file The file that we're to pass into the checker
+ * @returns {boolean|*} Checks if the file is correct
  */
-function handleError (err) {
-    console.log( err.toString() );
-    this.emit( 'end' );
+function isFixed( file ) {
+  return file.eslint !== null && file.eslint.fixed;
 }
 
 /**
  * Our SASS Compiler
- * @param $read
- * @param $write
- * @returns {Function}
+ * @param {string} $read The file we're looking at
+ * @param {string} $write The location of the output
+ * @returns {Function} The function that executes our compiler
  */
-function sassCompile ($read, $write) {
-    'use strict';
-    
-    const plugin_opts = [
-        autoprefixer( {
-            browsers: [ 'cover 99.5%' ],
-        } ),
-        cssnano(),
-    ];
-    
-    return function (done) {
-        
-        src( [ `${$read}` ] )
-            .pipe( plumber( {
-                errorHandler: handleError,
-            } ) )
-            .pipe( flatten() )
-            .pipe( sourcemaps.init() )
-            .pipe( globbing( {
-                extensions: '.scss',
-            } ) )
-            .pipe( sass().on( 'error', sass.logError ) )
-            .pipe( postcss( plugin_opts ) )
-            .pipe( rename( {
-                suffix: '.min',
-            } ) )
-            .pipe( sourcemaps.write( '.' ) )
-            .pipe( plumber.stop() )
-            .pipe( dest( $write ) );
-        done();
-    };
+function sassCompile( $read, $write ) {
+  const pluginOpts = [
+    autoprefixer( {
+      browsers: [ "cover 99.5%" ],
+    } ),
+    cssnano(),
+  ];
+  return function sassCompiler( done ) {
+    src( [ `${ $read }` ] )
+      .pipe( plumber( {
+        errorHandler: handleError,
+      } ) )
+      .pipe( flatten() )
+      .pipe( sourcemaps.init() )
+      .pipe( globbing( {
+        extensions: ".scss",
+      } ) )
+      .pipe( sass().on( "error", sass.logError ) )
+      .pipe( postcss( pluginOpts ) )
+      .pipe( rename( {
+        suffix: ".min",
+      } ) )
+      .pipe( sourcemaps.write( "." ) )
+      .pipe( plumber.stop() )
+      .pipe( dest( $write ) );
+    done();
+  };
 }
 
 /**
  * Our JS Compiler
- * @param $filename
- * @param $read
- * @param $write
- * @returns {Function}
+ * @param {string} $filename The filename we're watching
+ * @param {string} $read The location of our watcher
+ * @param {string} $write The output of our compiled file
+ * @returns {Function} The function that executes our compiler
  */
-function jsCompile ($filename, $read, $write) {
-    'use strict';
-    return function (done) {
-        src( [ `${$read}/**/*.js*` ] )
-            .pipe( plumber( {
-                errorHandler: handleError,
-            } ) )
-            .pipe( eslint( {
-                fix:   true,
-                quiet: true,
-            } ) )
-            .pipe( eslint.format() )
-            .pipe( eslint.results( results => {
-                // Called once for all ESLint results.
-                console.log( `${$filename} Results: ${results.length}` );
-                console.log( `Total Warnings: ${results.warningCount}` );
-                console.log( `Total Errors: ${results.errorCount}` );
-            } ) )
-            .pipe( eslint.failAfterError() )
-            .pipe( sourcemaps.init() )
-            .pipe( babel( {
-                presets: [
-                    '@babel/preset-env',
-                    '@babel/react',
-                ],
-            } ) )
-            .pipe( terser() )
-            .pipe( rename( {
-                suffix: '.min',
-            } ) )
-            .pipe( concat( $filename + '.min.js' ) )
-            .pipe( sourcemaps.write( './' ) )
-            .pipe( plumber.stop() )
-            .on( 'error', handleError )
-            .pipe( gulpIf( isFixed, dest( $write ) ) );
-            // .pipe( dest( $write ) );
-        done();
-    };
+function jsCompile( $filename, $read, $write ) {
+  return function jsCompiler( done ) {
+    src( [ `${ $read }/**/*.js*` ] )
+      .pipe( plumber( {
+        errorHandler: handleError,
+      } ) )
+      .pipe( eslint( {
+        fix: true,
+        quiet: true,
+      } ) )
+      .pipe( eslint.format() )
+      .pipe( eslint.results( ( results ) => {
+        // Called once for all ESLint results.
+        console.log( `${ $filename } Results: ${ results.length }` );
+        console.log( `Total Warnings: ${ results.warningCount }` );
+        console.log( `Total Errors: ${ results.errorCount }` );
+      } ) )
+      .pipe( eslint.failAfterError() )
+      .pipe( sourcemaps.init() )
+      .pipe( babel( {
+        presets: [
+          "@babel/preset-env",
+          "@babel/react",
+        ],
+      } ) )
+      .pipe( terser() )
+      .pipe( rename( {
+        suffix: ".min",
+      } ) )
+      .pipe( concat( `${ $filename }.min.js` ) )
+      .pipe( uglify() )
+      .pipe( sourcemaps.write( "./" ) )
+      .pipe( plumber.stop() )
+      .on( "error", handleError )
+      .pipe( gulpIf( isFixed, dest( $write ) ) );
+    done();
+  };
 }
 
-/**
- * Our checker to see if the file is fixed or not
- * @param file
- * @returns {boolean|*}
- */
-function isFixed (file) {
-    return file.eslint !== null && file.eslint.fixed;
-}
+// Function Variables
+const adminCSS = sassCompile( cssDec.admin.read, "./admin/dist" );
+const publicCSS = sassCompile( cssDec.public.read, writeDec.public );
+const adminJS = jsCompile(
+  jsDec.admin.fileName, jsDec.admin.read, "./admin/dist"
+);
+const publicJS = jsCompile(
+  jsDec.public.fileName, jsDec.public.read, writeDec.public
+);
 
 /**
  * Our File Watcher
  */
-function fileWatcher () {
-    watch( _css.admin.watch, adminCSS );
-    watch( _css.public.watch, publicCSS );
-    watch( _js.admin.watch, adminJS );
-    watch( _js.public.watch, publicJS );
+function fileWatcher() {
+  watch( cssDec.admin.watch, adminCSS );
+  watch( cssDec.public.watch, publicCSS );
+  watch( jsDec.admin.watch, adminJS );
+  watch( jsDec.public.watch, publicJS );
 }
 
-//</editor-fold>
+//  </editor-fold>
 
-/*======================================
-	Executions
-======================================*/
-
+//  Executions
 exports.js = series( parallel( adminJS, publicJS ) );
 exports.css = series( parallel( adminCSS, publicCSS ) );
 exports.admin = series( parallel( adminCSS, adminJS ) );
 exports.public = series( parallel( publicCSS, publicJS ) );
 exports.watcher = series( fileWatcher );
 exports.default = series(
-    parallel( adminCSS, publicCSS ),
-    parallel( adminJS, publicJS ),
-	fileWatcher,
+  parallel( adminCSS, publicCSS ),
+  parallel( adminJS, publicJS ),
+  fileWatcher
 );
